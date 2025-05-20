481b81ad62ba1bdbf3eb88132b8b3d8a1b3c15af
abdulkhanovmt <mtabdulkhanov@yandex.ru>
2025-05-20 15:52:33 +0300

    ADD general classes

--- src/main/java/com/javarush/island/abdulkhanov/creator/Creator.java
+++ src/main/java/com/javarush/island/abdulkhanov/creator/Creator.java
@@ -1,6 +1,8 @@
 package com.javarush.island.abdulkhanov.creator;
 
 
+import java.lang.reflect.InvocationTargetException;
+
 public abstract class Creator<T> {
-    public abstract T create(Class<? extends T> tClass);
+    public abstract T create(Class<? extends T> tClass) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
 }
--- src/main/java/com/javarush/island/abdulkhanov/creator/CreatorOfAnimal.java
+++ src/main/java/com/javarush/island/abdulkhanov/creator/CreatorOfAnimal.java
@@ -1,21 +1,25 @@
 package com.javarush.island.abdulkhanov.creator;
 
 import com.javarush.island.abdulkhanov.entity.animal.Animal;
+import com.javarush.island.abdulkhanov.entity.limit.Limit;
+import com.javarush.island.abdulkhanov.util.Randomiser;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class CreatorOfAnimal extends Creator<Animal>{
     @Override
-    public Animal create(Class<? extends Animal> animalClass) {
-        try {
-            Constructor<?> animalConstructor = animalClass.getConstructor();
-            Animal animal = (Animal) animalConstructor.newInstance();
-            animal.readConfig();
-
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public Animal create(Class<? extends Animal> animalClass) throws NoSuchMethodException,
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException {
+        Limit randomLimit = animalClass.getConstructor().newInstance().readConfig();
+        double maxWeight = Double.parseDouble(randomLimit.getMaxWeight());
+        double weight = Randomiser.getRandomWeight(maxWeight/1.5, maxWeight);
+        boolean gender = Randomiser.getRandomGender();
+        Animal randomAnimal = animalClass
+                .getConstructor(double.class, boolean.class, Limit.class)
+                .newInstance(weight, gender, randomLimit);
+        return randomAnimal;
     }
 }
